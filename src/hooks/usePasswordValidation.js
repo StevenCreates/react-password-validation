a3b813172f9c79de0d2c4f87a707cf2f24cefea4
StevenCreates <steven@stevencreates.tech>
2021-01-13 09:47:04 -0700

    updated hook to basic password validation

--- src/hooks/usePasswordValidation.js
+++ src/hooks/usePasswordValidation.js
@@ -1,44 +1,29 @@
 import { useState, useEffect } from "react";
+
 export const usePasswordValidation = ({
+  
   firstPassword = "",
   secondPassword = "",
   requiredLength = 8,
-  numberValidation = true,
-  lowercaseValidation = true,
-  uppercaseValidation = true,
-  specialcharacterValidation = true,
-  lengthValidation = true,
-}) => {
+    
+  }) => {
   const [validLength, setValidLength] = useState(null);
   const [hasNumber, setHasNumber] = useState(null);
   const [upperCase, setUpperCase] = useState(null);
   const [lowerCase, setLowerCase] = useState(null);
   const [specialChar, setSpecialChar] = useState(null);
   const [match, setMatch] = useState(null);
-
+    
   useEffect(() => {
-    lengthValidation &&
-      setValidLength(firstPassword.length >= requiredLength ? true : false);
-    uppercaseValidation &&
-      setUpperCase(firstPassword.toLowerCase() !== firstPassword);
-    lowercaseValidation &&
-      setLowerCase(firstPassword.toUpperCase() !== firstPassword);
-    numberValidation && setHasNumber(/\d/.test(firstPassword));
-    specialcharacterValidation &&
-      setSpecialChar(
-        /[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(firstPassword)
-      );
-    setMatch(firstPassword && firstPassword === secondPassword);
-  }, [
-    firstPassword,
-    secondPassword,
-    requiredLength,
-    lengthValidation,
-    uppercaseValidation,
-    lowercaseValidation,
-    numberValidation,
-    specialcharacterValidation,
-  ]);
-
+    
+  setValidLength(firstPassword.length >= requiredLength ? true : false);
+  setUpperCase(firstPassword.toLowerCase() !== firstPassword);
+  setLowerCase(firstPassword.toUpperCase() !== firstPassword);
+  setHasNumber(/\d/.test(firstPassword));
+  setMatch(firstPassword && firstPassword === secondPassword);
+  setSpecialChar(/[ `!@#$%^&*()_+\-=\]{};':"\\|,.<>?~]/.test(firstPassword));
+    
+  }, [firstPassword, secondPassword, requiredLength]);
+    
   return [validLength, hasNumber, upperCase, lowerCase, match, specialChar];
-};
+  };
